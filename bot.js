9ee4aede3fd5092765a6f1cd3f25bedc64998320
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-08-19 16:24:21 +0300

    added intents

--- bot.js
+++ bot.js
@@ -2,16 +2,46 @@
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+// client.on('message', (msg) => {
+//   console.log({ msg });
+//   // if (msg.content === 'ping') {
+//   msg.reply('pong');
+//   // }
+// });
+
+client.on('messageCreate', (message) => {
+  console.log({ message });
+  // if (message.author.bot) return;
+  if (message.content === 'ping') {
+    message.reply('pong');
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+// client.once('ready', () => {
+//   console.log('Ready!');
+// });
+
+// client.on('interactionCreate', (interaction) => {
+//   console.log(interaction);
+// });
+
+// client.on('messageCreate', (message) => {
+//   console.log(message);
+// });
+
+// client.login(process.env.DISCORD_TOKEN).then(console.log);
